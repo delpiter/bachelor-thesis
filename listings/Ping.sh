eea1c3c2795e54b1d59e09446b4a2658e83a2d2f
Gioele Foschi <gioele.foschi.14@gmail.com>
2026-08-22 12:34:29 +0200

    feat(media): added initial chapter listings and graphs

---
+++ listings/Ping.sh
@@ -0,0 +1,8 @@
+PING 8.8.8.8 (8.8.8.8) 56(84) bytes of data.
+64 bytes from 8.8.8.8: icmp_seq=1 ttl=111 time=71.1 ms
+64 bytes from 8.8.8.8: icmp_seq=2 ttl=111 time=53.9 ms
+64 bytes from 8.8.8.8: icmp_seq=3 ttl=111 time=42.8 ms
+
+--- 8.8.8.8 ping statistics ---
+3 packets transmitted, 3 received, 0% packet loss, time 2000ms
+rtt min/avg/max/mdev = 42.838/55.945/71.057/11.607 ms
